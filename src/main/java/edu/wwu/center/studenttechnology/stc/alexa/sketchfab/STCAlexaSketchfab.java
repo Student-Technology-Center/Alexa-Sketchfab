ddbb029d54fdc7bab8d4583e833adaf42f594bfa
Alex <AlexAyala9975@hotmail.com>
2017-02-26 23:33:13 -0800

    Remove unuse assets

--- src/main/java/edu/wwu/center/studenttechnology/stc/alexa/sketchfab/STCAlexaSketchfab.java
+++ src/main/java/edu/wwu/center/studenttechnology/stc/alexa/sketchfab/STCAlexaSketchfab.java
@@ -1,11 +1,6 @@
 package edu.wwu.center.studenttechnology.stc.alexa.sketchfab;
 
-import com.amazon.speech.speechlet.LaunchRequest;
-import com.amazon.speech.speechlet.Session;
-import com.amazon.speech.speechlet.SpeechletException;
-
 import edu.wwu.center.studenttechnology.stc.alexa.framework.speechlet.Speechlet;
-import edu.wwu.center.studenttechnology.stc.alexa.framework.speechlet.SpeechletResponse;
 import edu.wwu.center.studenttechnology.stc.alexa.sketchfab.database.DatabaseHandler;
 import edu.wwu.center.studenttechnology.stc.alexa.sketchfab.intent.SketchfabRequestIntent;
 
@@ -13,7 +8,6 @@ import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 
 public class STCAlexaSketchfab extends Speechlet {
-    private final Logger log;
     private final DatabaseHandler databaseHandler;
 
     public STCAlexaSketchfab(Logger log) {
